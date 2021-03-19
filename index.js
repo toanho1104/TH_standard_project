4632ce53dd08c60b077fae0fdf893f291c6efe22
toanho1104 <toanho1104@gmail.com>
2021-03-19 23:14:54 +0700

    add_reactotron

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native'
+import App from './App'
+import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(appName, () => App);
+// eslint-disable-next-line no-undef
+if (__DEV__) {
+  import('./src/configs/ReactotronConfig').then(() => console.log('Reactotron Configured'))
+} else {
+  console.tron = { log: () => true }
+}
+
+AppRegistry.registerComponent(appName, () => App)
